37e597dfe1f29363dc3fe53a0cbbab5af0a68ddb
Prism <whitesky554221@gmail.com>
2023-11-27 11:00:02 +0700

    Add files via upload

---
+++ 3.6.5 Add Comments!.py
@@ -0,0 +1,12 @@
+"""
+Asks user for first name, 
+middle name, 
+and last name.
+"""
+first_name = input("Enter your first name: ")
+middle_name = input("Enter your middle name: ")
+last_name = input("Enter your last name: ")
+
+#Combines first name, middle name, and last name
+full_name = first_name + " " + middle_name + " " + last_name
+print(full_name)
---
+++ 3.7.3 Statically Typed Languages.java
@@ -0,0 +1,8 @@
+public class MyProgram
+{
+    public static void main(String[] args)
+    {
+        int x = 10;     // x is an integer
+        String message = "Hello";   // message is a string
+    }
+}
