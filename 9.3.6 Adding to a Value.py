02524b4c8f5e36990559d93c4eed878fb6eebe90
Soveraint <whitesky554221@gmail.com>
2024-01-16 10:26:02 +0700

    Add files via upload

---
+++ 9.3.6 Adding to a Value.py
@@ -0,0 +1,9 @@
+num1 = 10
+
+# Your code here...
+def add_to_num1():
+    user_input = float(input("Enter a number: "))  
+    result = num1 + user_input 
+    print(f"The result is: {result}")
+
+add_to_num1()
