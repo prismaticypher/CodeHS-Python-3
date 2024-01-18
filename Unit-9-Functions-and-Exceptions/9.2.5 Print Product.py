571ffdac300f598ace26e6a40c167c93110e397d
Soveraint <whitesky554221@gmail.com>
2024-01-18 22:25:08 +0700

    Add files via upload

---
+++ Unit-9-Functions-and-Exceptions/9.2.5 Print Product.py
@@ -0,0 +1,5 @@
+def print_product(x, y):
+    product = x * y
+    print(f"The product of {x} and {y} is: {product}")
+    
+print_product(4, 7)
---
+++ Unit-9-Functions-and-Exceptions/9.4.4 Add One.py
@@ -0,0 +1,6 @@
+def add_one(number):
+    return number + 1
+
+result = add_one(5)
+
+print(result)
