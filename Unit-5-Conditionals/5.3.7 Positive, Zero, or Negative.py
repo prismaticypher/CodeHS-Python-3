35871338edcdb600e1b53e12e91258f55f312294
Soveraint <whitesky554221@gmail.com>
2024-01-18 22:15:37 +0700

    Add files via upload

---
+++ Unit-5-Conditionals/5.3.7 Positive, Zero, or Negative.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: "))
+
+if number > 0: 
+    print("That number is positive!")
+elif number < 0:
+    print("That number is negative!")
+else:
+    print("That number is zero!")
